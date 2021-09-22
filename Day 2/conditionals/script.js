c61381095eeb1386b35b230eb5e1d8c47e376515
Aakash Malhotra <aakash@koverhoop.com>
2021-09-22 19:59:52 -0400

    Day-2

---
+++ Day 2/conditionals/script.js
@@ -0,0 +1,7 @@
+function sampleFunction() {
+    let age = 10;
+
+    if (age < 18) {
+        console.log("You are not allowed to drink.");
+    }
+}
---
+++ Day 2/number comparison/script.js
@@ -0,0 +1,16 @@
+function sampleFunction() {
+    let num1 = 10;
+    let num2 = 10;
+
+    if (num1 > num2) {
+        console.log("hey num1 is greater");
+    }
+
+    if (num2 > num1) {
+        console.log("hey num2 is greater");
+    }
+
+    if (num1 == num2) {
+        console.log("Both are equal");
+    }
+}
