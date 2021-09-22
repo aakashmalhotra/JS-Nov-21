2ca633ad613a41b0f88d9cbdf859333429b6b26b
Aakash Malhotra <aakash@koverhoop.com>
2021-09-22 19:01:47 -0400

    Day-2

---
+++ Day 2/escape-strings/script.js
@@ -0,0 +1,5 @@
+function sampleFunction() {
+    console.log("This is a paragraph that I am typing \nwhich may make no sense because I just need to type something.\nIt\'s an awesome day.\nI am able to print this way because I have the help of \\")
+
+    // try out \t on your own
+}
