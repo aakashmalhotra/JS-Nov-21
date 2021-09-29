b02754376100d0e54b3521b11c4dbf5a4cbfeb75
Aakash Malhotra <aakash@koverhoop.com>
2021-09-29 19:52:41 -0400

    Day-3

--- Day 3/Or operator/script.js
+++ Day 3/Or operator/script.js
@@ -16,3 +16,22 @@ function sampleFunction() {
         console.log("Do you even wanna buy anything?")
     }
 }
+
+// you can rent a ferrari if any one of the following is true:
+// - you are more than 25 years old 
+// - you are a student of concordia university
+// - you have a million dollars
+// - you want to rent a ferrari desperately
+// - you have driving experience of more than 10 years
+
+// ==========================================================================
+// let age = 50;
+// let name = "robinhood";
+// let isNice = true;
+// let isprogrammer = false;
+
+// Evaluate the result of statements below with the above values:
+// 1. (age < 25 && name =="robinhood" || isNice)
+// 2. (age < 25 && (name =="robinhood" || isNice))
+// 3. (isprogrammer || isNice)
+
