308aead6faa1484d986c6d500204a469e9bfe89b
Aakash Malhotra <aakash@koverhoop.com>
2021-09-29 18:49:16 -0400

    Day-3

---
+++ Day 3/logical operators/script.js
@@ -0,0 +1,22 @@
+function sampleFunction() {
+    let age = 25;
+    let drivingExperience = 3;
+
+    if (age > 25 && drivingExperience >= 5) {
+        console.log("You can rent a ferrari");
+    }
+    else if (age > 25 && drivingExperience < 5) {
+        console.log("You can rent a Audi Q5");
+    }
+    else {
+        console.log("You can rent a mazda 3");
+    }
+}
+
+// Write a program to find the best toy for a dog:
+// If the dog weighs more than 60 kg and likes to chew suggest "ultra chew - $10"
+// If the dog weighs more than 60 kg and doesn't like to chew suggest "super chew - $7"
+// If the dog weighs less than 60 kg and likes to chew suggest "super chew - $7"
+// If the dog weighs less than 60 kg and doesn't like to chew suggest "light chew - $3"
+
+
