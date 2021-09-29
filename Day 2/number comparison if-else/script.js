e62c70d1b4be1f37044ed10d9c12e905473cda24
Aakash Malhotra <aakash@koverhoop.com>
2021-09-29 18:28:33 -0400

    Day-2

--- Day 2/number comparison if-else/script.js
+++ Day 2/number comparison if-else/script.js
@@ -2,24 +2,14 @@ function sampleFunction() {
     let num1 = 10;
     let num2 = 10;
 
-    // if (num1 > num2) {
-    //     console.log("hey num1 is greater");
-    // }
-    // else {
-    //     console.log("hey num1 is not greater than num2");
-    // }
-
-
-    // -----------------------
     if (num1 > num2) {
         console.log("hey num1 is greater");
     }
-    else {
-        if (num2 > num1) {
-            console.log("hey num2 is not greater than num1");
-        }
-        else {
-            console.log("Both are equal");
-        }
+    else if (num2 > num1) {
+        console.log("hey num2 is not greater than num1");
     }
+    else {
+        console.log("Both are equal");
+    }
+
 }
