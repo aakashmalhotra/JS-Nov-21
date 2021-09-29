8ac152b70c439ae36092a6f7ced36ef0d215206b
Aakash Malhotra <aakash@koverhoop.com>
2021-09-29 19:39:18 -0400

    Day-3

--- Day 3/Or operator/script.js
+++ Day 3/Or operator/script.js
@@ -1,3 +1,18 @@
+
 function sampleFunction() {
-    console.log("function called")
+    let dogWeight = 65;
+    let likesToChew = true;
+
+    if (dogWeight >= 60 && likesToChew) {
+        console.log("ultra chew - $10");
+    }
+    else if ((dogWeight >= 60 && !likesToChew) || (dogWeight < 60 && likesToChew)) {
+        console.log("super chew - $7");
+    }
+    else if (dogWeight < 60 && !likesToChew) {
+        console.log("light chew - $3");
+    }
+    else {
+        console.log("Do you even wanna buy anything?")
+    }
 }
--- Day 3/logical operators/script.js
+++ Day 3/logical operators/script.js
@@ -14,9 +14,41 @@ function sampleFunction() {
 }
 
 // Write a program to find the best toy for a dog:
-// If the dog weighs more than 60 kg and likes to chew suggest "ultra chew - $10"
-// If the dog weighs more than 60 kg and doesn't like to chew suggest "super chew - $7"
+// If the dog weighs more than or equal to 60 kg and likes to chew suggest "ultra chew - $10"
+// If the dog weighs more than or equal to 60 kg and doesn't like to chew suggest "super chew - $7"
 // If the dog weighs less than 60 kg and likes to chew suggest "super chew - $7"
 // If the dog weighs less than 60 kg and doesn't like to chew suggest "light chew - $3"
 
 
+function dogToySuggester() {
+    let dogWeight = 65;
+    let likesToChew = true;
+
+    if (dogWeight >= 60 && likesToChew) {
+        //  if (dogWeight >= 60 && likesToChew == true)
+        //  if (65 >= 60 && true == true)
+        //  if (true && true)
+        // if(true)
+
+        // Option 2
+        // if (dogWeight >= 60 && likesToChew)
+        // if (65 >= 60 && true)
+        // if (true && true)
+        // if(true)
+
+        console.log("ultra chew - $10");
+    }
+
+    else if (dogWeight >= 60 && !likesToChew) {
+        console.log("super chew - $7");
+    }
+    else if (dogWeight < 60 && likesToChew) {
+        console.log("super chew - $7");
+    }
+    else if (dogWeight < 60 && !likesToChew) {
+        console.log("light chew - $3");
+    }
+    else {
+        console.log("Do you even wanna buy anything?")
+    }
+}
