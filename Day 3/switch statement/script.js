4549864da3b2ee2f043440413cc49e833f50818d
Aakash Malhotra <aakash@koverhoop.com>
2021-09-29 20:17:15 -0400

    Day-3

--- Day 3/switch statement/script.js
+++ Day 3/switch statement/script.js
@@ -1,3 +1,49 @@
+function sameFunctionalityWithElseIf() {
+    optionSelected = 1;
+
+    console.log("Possible options\n1. Add \n2. Subt \n3. Multiply \n4. Divide");
+
+    if (optionSelected == 1) {
+        console.log(`The option you selected is ${optionSelected}`);
+    }
+    else if (optionSelected == 2) {
+        console.log(`The option you selected is ${optionSelected}`);
+    }
+    else if (optionSelected == 3) {
+        console.log(`The option you selected is ${optionSelected}`);
+    }
+    else if (optionSelected == 4) {
+        console.log(`The option you selected is ${optionSelected}`);
+    }
+    else {
+        console.log(`You don't know how to use a calculator?`);
+    }
+}
+
 function sampleFunction() {
-    console.log("function called")
+    optionSelected = 2;
+
+    console.log("Possible options\n1. Add \n2. Subt \n3. Multiply \n4. Divide");
+
+    switch (optionSelected) {
+        case 1: {
+            console.log(`The option you selected is 1`);
+            break;
+        }
+        case 2: {
+            console.log(`The option you selected is 2`);
+            break;
+        }
+        case 3: {
+            console.log(`The option you selected is 3`);
+            break;
+        }
+        case 4: {
+            console.log(`The option you selected is 4`);
+            break;
+        }
+        default: {
+            console.log(`You don't know how to use a calculator?`);
+        }
+    }
 }
