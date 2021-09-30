488712faa65be9f1c2c0c610922348a048fe54d4
Aakash Malhotra <aakash@koverhoop.com>
2021-09-29 21:23:45 -0400

    Day-3

---
+++ Day 3/loops/script.js
@@ -0,0 +1,9 @@
+function sampleFunction() {
+    let runNumber = 1; // initialisation
+    let noOfRuns = 5;
+
+    while (runNumber <= noOfRuns) { // condition
+        console.log(`Hi! This is run number ${runNumber} trying to print Aakash`);
+        runNumber = runNumber + 1; // increament
+    }
+}
--- Day 3/switch statement/script.js
+++ Day 3/switch statement/script.js
@@ -4,24 +4,33 @@ function sameFunctionalityWithElseIf() {
     console.log("Possible options\n1. Add \n2. Subt \n3. Multiply \n4. Divide");
 
     if (optionSelected == 1) {
-        console.log(`The option you selected is ${optionSelected}`);
+        console.log(`The option you selected is 1`);
     }
     else if (optionSelected == 2) {
-        console.log(`The option you selected is ${optionSelected}`);
+        console.log(`The option you selected is 2`);
     }
     else if (optionSelected == 3) {
-        console.log(`The option you selected is ${optionSelected}`);
+        console.log(`The option you selected is 3`);
     }
     else if (optionSelected == 4) {
-        console.log(`The option you selected is ${optionSelected}`);
+        console.log(`The option you selected is 4`);
     }
     else {
         console.log(`You don't know how to use a calculator?`);
     }
 }
 
+
+// Extend the functionality of the switch case.
+// Build a calculator that has 2 variables(num1, num2) containing two numbers
+// and another variable that contains one of the four operations Add, Subtract, multiply and divide.
+// Depending on the optionSelected the respecting operation should be performed.
+// For example: if optionSelected = 3, num1=5 and num2=10. Num1 and num2 should be multiplied and result 50 should be displayed.
+
 function sampleFunction() {
-    optionSelected = 2;
+    let num1 = 20;
+    let num2 = 10;
+    optionSelected = 3;
 
     console.log("Possible options\n1. Add \n2. Subt \n3. Multiply \n4. Divide");
 
