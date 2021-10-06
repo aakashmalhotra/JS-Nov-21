b20a034ac31183e1f933132648d8db3fd642080b
Aakash Malhotra <aakash@koverhoop.com>
2021-10-06 19:20:44 -0400

    Day-4

--- Day 4/for loop/script.js
+++ Day 4/for loop/script.js
@@ -1,3 +1,26 @@
 function sampleFunction() {
-    console.log("function called")
+    let rowNumber;
+
+    for (rowNumber = 1; rowNumber <= 10; rowNumber++) {
+        console.log(`5 * ${rowNumber} = ${5 * rowNumber}`);
+    }
 }
+
+function sampleFunctionAlternate() {
+
+    for (let rowNumber = 1; rowNumber <= 10; rowNumber++) {
+        console.log(`5 * ${rowNumber} = ${5 * rowNumber}`);
+    }
+}
+
+// Practice question 
+// Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+
+
+// Sample Output :
+// "0 is even"
+// "1 is odd"
+// "2 is even"
+
+// 10 % 2 == 0 // even
+
---
+++ Day 4/functions/script.js
@@ -0,0 +1,9 @@
+function sampleFunction() {
+    console.log("function called");
+
+}
+
+// Defining a function
+function sayHello() {
+    console.log("hi! John");
+}
