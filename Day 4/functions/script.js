9da5374ad8e51f685ee8d829c42b313e49ac4706
Aakash Malhotra <aakash@koverhoop.com>
2021-10-06 19:46:11 -0400

    Day-4

--- Day 4/functions/script.js
+++ Day 4/functions/script.js
@@ -1,9 +1,20 @@
 function sampleFunction() {
     console.log("function called");
-
+    sayHello("John"); // function call
+    sayHello("Jane");
+    sayHello("Spiderman");
+    console.log("say hello has been called. This is the end of the program");
 }
 
 // Defining a function
-function sayHello() {
-    console.log("hi! John");
+function sayHello(name) {
+    delimeterPrinter('#');
+    console.log(`**** Hi! ${name} ****`);
+    delimeterPrinter('*');
 }
+
+function delimeterPrinter() {
+    console.log("------------------------------");
+}
+
+// Change function delimeterPrinter so that it takes a symbol that would be printer as a delimter 20 times.
