c769a485ae8ed0952c7065076b5b55758c8654c4
Aakash Malhotra <aakash@koverhoop.com>
2021-10-13 21:24:28 -0400

    Day-5

---
+++ Day 5/array/script.js
@@ -0,0 +1,15 @@
+function sampleFunction() {
+    const student1 = "John";
+    const student2 = "Jane";
+    const student3 = "spiderman";
+    const student4 = "dsfsda";
+    const student5 = "dsfsda";
+
+    const students = ["John", "Jane", "spiderman", "dsfsda", "dsfsda"];
+    const numbers = [5, 10, 56, 76, 3, 7, 84];
+
+    for (let i = 0; i < students.length; i++) {
+        console.log(students[i]);
+    }
+
+}
