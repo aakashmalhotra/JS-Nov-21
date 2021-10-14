184006ba079e46690b63e443621b2bb3c1d9937c
Aakash Malhotra <aakash@koverhoop.com>
2021-10-13 20:09:12 -0400

    Day-5

--- Day 5/drink decider/script.js
+++ Day 5/drink decider/script.js
@@ -14,10 +14,21 @@ function decideTheDrink(event) {
     const resultDivEl = document.getElementById("result");
 
 
-    if (age < 18) {
+    if (age <= 12) {
+        resultDivEl.textContent = "You cannot enter";
+        resultDivEl.style.color = "red";
+    }
+    else if (age < 18) {
+        // Manipulate the content
         resultDivEl.textContent = "You can only have water";
+        resultDivEl.style.color = "blue";
     }
     else {
         resultDivEl.textContent = "You can have beer";
+        resultDivEl.style.color = "blue";
     }
+
+    // Manipulate CSS
+    resultDivEl.style.fontSize = "16px";
+    resultDivEl.style.padding = "12px";
 }
