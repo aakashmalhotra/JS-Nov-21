d5280c7582de927b75d8b54540a4627df489c305
Aakash Malhotra <aakash@koverhoop.com>
2021-10-27 20:00:32 -0400

    day-7

--- Day 7/watch object/script.js
+++ Day 7/watch object/script.js
@@ -1,14 +1,31 @@
 const clock = {
-    color: "black",
+    color: "orange",
     time: "8:00 PM",
     brand: "Rolex",
+    background: "green",
+    activateDarkMode: function () {
+        this.color = "floralwhite";
+        this.background = "dimgrey";
+    }
 }
 
 function displayproperties() {
     const resultEl = document.getElementById("result");
 
     resultEl.innerHTML = `
-        Color of the clock: ${clock.color}
+        Color of the clock: ${clock.color} <br>
+        Time right now: ${clock.time} <br>
+        Brand: ${clock.brand}
     `
+}
 
+function updateTime() {
+    const watchTimeEl = document.getElementById("watch-time");
+    watchTimeEl.innerHTML = clock.time;
+}
+
+function updateColor() {
+    const watchTimeEl = document.getElementById("watch-time");
+    watchTimeEl.style.color = clock.color;
+    watchTimeEl.style.backgroundColor = clock.background;
 }
