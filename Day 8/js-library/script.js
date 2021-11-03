f658d908a13108da167829dac533ec97695a4169
Aakash Malhotra <aakash@koverhoop.com>
2021-11-03 19:09:27 -0400

    day-8

---
+++ Day 8/js-library/script.js
@@ -0,0 +1,15 @@
+new TypeIt("#my-element", {
+    strings: "This is what will be typed!",
+})
+    .pause(2000)
+    .type(" What are you waiting for ? Go do some cool stuff with this library")
+    .break()
+    .pause(1000)
+    .type(" I can't believe you are still here")
+    .break()
+    .pause(2000)
+    .type(" OMG! You won't leave. I am gonna leave. Bye!")
+    .break()
+    .pause(2000)
+    .empty()
+    .go();
