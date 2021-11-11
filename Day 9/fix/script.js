6a0f6eb2af1a3f714b6366fa9b9f5a91b217dba9
Aakash Malhotra <aakash@koverhoop.com>
2021-11-10 21:13:48 -0500

    day-9

---
+++ Day 9/fix/script.js
@@ -0,0 +1,25 @@
+let clothingItems = [];
+
+function listOfClothes(event) {
+    event.preventDefault();
+    console.log("Function getting called");
+    //1.Fetch values from the page/dom/document/html/form
+    const clothingNameEl = document.getElementById("name-clothe");
+    const clothingTypeEl = document.getElementById("type-clothe");
+    const clothingCostEl = document.getElementById("cost-clothe");
+
+    const clothingName = clothingNameEl.value;
+    const clothingType = clothingTypeEl.value;
+    const clothingCost = clothingCostEl.value;
+
+    //2.create the object
+    const item = {
+        name: clothingName,
+        type: clothingType,
+        cost: clothingCost,
+    }
+
+    // 3. Self-testing - does it log an item containing expected keys and values?
+    console.log(item);
+
+}
--- Day 9/practice/script.js
+++ Day 9/practice/script.js
@@ -1,18 +1,12 @@
-let clothingItems = [];
+let myClothingList = [];
 
 function listOfClothes(event) {
     event.preventDefault();
     console.log("Function getting called");
 
-    // 1. Fetch values from the page/dom/document/html/form
-
-    const clothingNameEl = document.getElementById("name-clothe");
-    const clothingTypeEl = document.getElementById("type-clothe");
-    const clothingCostEl = document.getElementById("cost-clothe");
-
-    const clothingName = clothingNameEl.value;
-    const clothingType = clothingTypeEl.value;
-    const clothingCost = clothingCostEl.value;
+    const clothingName = $("#name-clothe").val();
+    const clothingType = $("#type-clothe").val();
+    const clothingCost = $("#cost-clothe").val();
 
     // 2. Create the object
     const item = {
@@ -24,9 +18,25 @@ function listOfClothes(event) {
     // 3. Self-testing - does it log an item containing expected keys and values?
     console.log(item);
 
-    clothingNameEl.value = "";
-    clothingTypeEl.value = "";
-    clothingCostEl.value = "";
+    $("#name-clothe").val("");
+    $("#type-clothe").val("");
+    $("#cost-clothe").val("");
 
+    myClothingList.push(item);
 
+    printAllItems();
+
+}
+
+function printAllItems() {
+    const resultEl = document.getElementById("result");
+
+    let resultHtml = "";
+
+    for (let i = 0; i < myClothingList.length; i++) {
+        resultHtml = resultHtml + `<li> Name: ${myClothingList[i].name} / Cost: ${myClothingList[i].cost} / Type: ${myClothingList[i].type}</li>`;
+
+    }
+
+    resultEl.innerHTML = resultHtml;
 }
