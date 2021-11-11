4266582c24be66d033fb4724fd367b230404132c
Aakash Malhotra <aakash@koverhoop.com>
2021-11-10 19:37:05 -0500

    day-9

--- Day 9/practice/script.js
+++ Day 9/practice/script.js
@@ -1,3 +1,32 @@
-function sampleFunction() {
-    console.log("function called")
+let clothingItems = [];
+
+function listOfClothes(event) {
+    event.preventDefault();
+    console.log("Function getting called");
+
+    // 1. Fetch values from the page/dom/document/html/form
+
+    const clothingNameEl = document.getElementById("name-clothe");
+    const clothingTypeEl = document.getElementById("type-clothe");
+    const clothingCostEl = document.getElementById("cost-clothe");
+
+    const clothingName = clothingNameEl.value;
+    const clothingType = clothingTypeEl.value;
+    const clothingCost = clothingCostEl.value;
+
+    // 2. Create the object
+    const item = {
+        name: clothingName,
+        type: clothingType,
+        cost: clothingCost,
+    }
+
+    // 3. Self-testing - does it log an item containing expected keys and values?
+    console.log(item);
+
+    clothingNameEl.value = "";
+    clothingTypeEl.value = "";
+    clothingCostEl.value = "";
+
+
 }
